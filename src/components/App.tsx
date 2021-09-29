c5c217c06f3baf94c4c5551660e930e3a9cfd238
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-09-30 00:49:57 +0200

    feat: Add basic key generation example

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,38 @@
-import { Button } from "@patternfly/react-core";
+import { Button, TextArea } from "@patternfly/react-core";
+import * as openpgp from "openpgp";
+import { useState } from "react";
 
 export function App() {
-  return <Button variant="primary">Hello, gridge!</Button>;
+  const [generatedKey, setGeneratedKey] = useState("");
+
+  return (
+    <div>
+      <Button
+        variant="primary"
+        onClick={async () => {
+          const key = await openpgp.generateKey({
+            userIds: [
+              {
+                name: "Felicitas Pojtinger",
+                email: "felicitas@pojtinger.com",
+              },
+            ],
+            passphrase: "123456",
+          });
+
+          setGeneratedKey(key.privateKeyArmored);
+        }}
+      >
+        Generate key
+      </Button>
+
+      {generatedKey !== "" && (
+        <TextArea
+          value={generatedKey}
+          aria-label="Generated PGP key"
+          rows={generatedKey.split("\n").length}
+        />
+      )}
+    </div>
+  );
 }
