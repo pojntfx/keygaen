45e4ffa71adc42788a4feb95942e60e8590872e1
Felix Pojtinger <felix@pojtinger.com>
2021-10-07 04:50:14 +0200

    feat: Add encryption option checkbox and public key selector

--- pkg/stories/encrypt_and_sign_modal.go
+++ pkg/stories/encrypt_and_sign_modal.go
@@ -15,7 +15,18 @@ func (c *EncryptAndSignModalStory) Render() app.UI {
 	return c.WithRoot(
 		&components.EncryptAndSignModal{
 			PrivateKeys: []components.EncryptionKey{},
-			PublicKeys:  []components.EncryptionKey{},
+			PublicKeys: []components.EncryptionKey{
+				{
+					ID:       "123456",
+					FullName: "Alice Example",
+					Email:    "alice@example.com",
+				},
+				{
+					ID:       "319312",
+					FullName: "Bob Example",
+					Email:    "bob@example.com",
+				},
+			},
 
 			OnSubmit: func(file []byte, publicKeyID, privateKeyID string) {
 				app.Window().Call("alert", fmt.Sprintf("Encrypted and signed file %v, using public key ID %v and private key ID %v", file, publicKeyID, privateKeyID))
