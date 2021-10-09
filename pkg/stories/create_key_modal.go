2d498aa6a1327fabcb7093c191b7aeadec309ced
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-10-09 19:24:07 +0200

    feat: Start using modals for modal body content

--- pkg/stories/create_key_modal.go
+++ pkg/stories/create_key_modal.go
@@ -9,15 +9,37 @@ import (
 
 type CreateKeyModalStory struct {
 	Story
+
+	modalOpen bool
 }
 
 func (c *CreateKeyModalStory) Render() app.UI {
+	c.EnableShallowReflection()
+
 	return c.WithRoot(
-		&components.CreateKeyModal{
-			OnSubmit: func(fullName, email, _ string) {
-				app.Window().Call("alert", fmt.Sprintf("Created key with full name %v, email %v and a password", fullName, email))
-			},
-			OnCancel: func() {},
-		},
+		app.Div().Body(
+			app.Button().
+				Class("pf-c-button pf-m-primary").
+				Type("button").
+				Text("Create Key").
+				OnClick(func(ctx app.Context, e app.Event) {
+					c.modalOpen = !c.modalOpen
+				}),
+			app.If(
+				c.modalOpen,
+				&components.CreateKeyModal{
+					OnSubmit: func(fullName, email, _ string) {
+						app.Window().Call("alert", fmt.Sprintf("Created key with full name %v, email %v and a password", fullName, email))
+
+						c.modalOpen = false
+					},
+					OnCancel: func() {
+						c.modalOpen = false
+
+						c.Update()
+					},
+				},
+			),
+		),
 	)
 }
