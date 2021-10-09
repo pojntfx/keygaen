be8914c3279ce152890ccc2cb3f3a22b3a2fc000
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-10-09 16:47:47 +0200

    feat: Implement generic success modal

---
+++ pkg/stories/success_modal.go
@@ -0,0 +1,47 @@
+package stories
+
+import (
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+	"github.com/pojntfx/gridge/pkg/components"
+)
+
+type SuccessModalStory struct {
+	Story
+
+	modalOpen bool
+}
+
+func (c *SuccessModalStory) Render() app.UI {
+	return c.WithRoot(
+		app.Div().Body(
+			app.Button().
+				Class("pf-c-button pf-m-primary").
+				Type("button").
+				Text("Open Success Modal").
+				OnClick(func(ctx app.Context, e app.Event) {
+					c.modalOpen = true
+				}),
+			app.If(
+				c.modalOpen,
+				&components.SuccessModal{
+					ID:          "success-modal-story",
+					Icon:        "fas fa-check",
+					Title:       "Key successfully generated!",
+					Body:        "It has been added to the key list.",
+					ActionLabel: "Continue to key list",
+
+					OnClose: func() {
+						c.modalOpen = false
+
+						c.Update()
+					},
+					OnAction: func() {
+						c.modalOpen = false
+
+						c.Update()
+					},
+				},
+			),
+		),
+	)
+}
